bec8ffaf939c8d7f5f391dc65cb88976f7c1abf9
dnalren <nerlandar@gmail.com>
2015-03-17 10:55:01 -0600

    added code for makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+##cacheMatrix caches a variable for a matriz and a variable for its inverse
+## and contains functions to set and get the cached variables
+## args: invertible numeric matrix
+## returns: list of functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inv <- NULL
+	set <- function(y) {
+		x <<- y
+		inv <<-NULL
+	}
+	get <- function()x
+	setinv <- function(inverse) inv <<- inv
+	getinv <- function() inv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
+	
+	
 }
+
 
 
 ## Write a short comment describing this function
